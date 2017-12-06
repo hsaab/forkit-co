329030c3901aa4ca01b466a122422ab1bf413407
Hassan Saab <hassansaab@Hassans-MacBook-Pro.local>
2017-12-06 10:07:35 -0800

    deleted password recovery

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Home from './Containers/Home';
 import About from './Containers/About';
 import Contact from './Containers/Contact';
-import PasswordRecovery from './Containers/PasswordRecovery';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -14,7 +13,6 @@ class App extends Component {
           <Route exact path='/' component={Home}/>
           <Route path="/about" component={About}/>
           <Route path="/contact" component={Contact}/>
-          <Route path="/recovery" component={PasswordRecovery}/>
         </Switch>
       </HashRouter>
     );
