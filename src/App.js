1ebf2b28624c2fbd2612d6c4384aff09c6dbc391
Hassan Saab <hassansaab@Hassans-MacBook-Pro.local>
2017-12-06 10:14:08 -0800

    added pass recovery

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Containers/Home';
 import About from './Containers/About';
 import Contact from './Containers/Contact';
+import PasswordRecovery from './Containers/PasswordRecovery';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
           <Route exact path='/' component={Home}/>
           <Route path="/about" component={About}/>
           <Route path="/contact" component={Contact}/>
+          <Route path="/recovery" component={PasswordRecovery}/>
         </Switch>
       </HashRouter>
     );
