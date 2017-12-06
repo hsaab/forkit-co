c51a97e0328cc20ac76f98686d74fbd5c2722ef8
Hassan Saab <hassansaab@Hassans-MacBook-Pro.local>
2017-12-06 11:38:37 -0800

    added brandon bio

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -33,10 +33,14 @@ class About extends Component {
                    key product themes and strategies, while supporting the front-end and back-end team in execution and
                    implementation. Previously, Hassan worked in investment banking, focusing on business technology M&A and
                    strategic advisory. Hassan graduated from the Wharton School of Business with a concentration in Finance. In
-                   his spare time, he enjoys creating and mixing a wide range of music (classical, EDM, etc.) and playing sports.`}/>
+                   his spare time, he creates a wide range of music (classical, EDM, etc.) and plays sports.`}/>
               </Col>
               <Col xs={{ size: 5, order: 3 }}>
-                <Profile image={hassan} name={'Brandon'} title={'Co-Founder'} bio={'Hassan Saab loves kbbq'}/>
+                <Profile image={hassan} name={'Brandon'} title={'Co-Founder'} bio={`Brandon is a frontend-focused developer with a
+                  keen sense for UI/UX layout and design. He originally created the idea for Fork It in preparation for a hackathon.
+                  Prior to becoming a software engineer, Brandon was a business student at Washington University in St. Louis Olin
+                  Business School with healthcare and management consulting experience. Outside of coding, Brandon is an avid skiier
+                  and outdoor adventurer. His dream is to one day be invited to perform on stage with Chance The Rapper.`}/>
                 <Profile image={hassan} name={'Paul'} title={'Co-Founder'} bio={`Paul is one of the primary backend developers
                   who provides support and insight on systems design decisions. Previously, Paul worked as a freelance full stack
                   developer and as a product designer at various start ups in the Bay Area. Paul is currently a Computer Engineering
